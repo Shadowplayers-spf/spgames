8e761d74ea6b4927e4cbd8824eae50b9b9ed2f2c
Jasdoge <jasdoge@gmail.com>
2025-12-06 16:26:17 +0200

    prep

--- server/jeopardy/_game.js
+++ server/jeopardy/_game.js
@@ -1,1 +1,32 @@
+import Board from "./Board.js";
+import Game from "./Game.js";
+import Category from "./Category.js";
+import Question from "./Question.js";
+import Collaborator from "./Collaborator.js";
 
+export default class Jeopardy{
+	rest = null;
+
+	constructor( rest ){
+		
+		this.rest = rest;
+
+
+	}
+
+	async usrGetGames(){
+
+		// Todo: Continue here
+
+	}
+
+	async usrGetBoards(){
+
+	}
+
+	async usrGetBoardData( boardId ){
+
+
+	}
+
+}
